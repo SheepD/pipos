14e82a9c4fddc937e2b583bb4065faea895500e1
Maru de Vera <maru@sheepd.com>
2019-11-21 11:57:59 +0800

    * include vue boilerplate code * do not display menu bar * update readme

--- README.md
+++ README.md
@@ -10,14 +10,15 @@ A basic Point of Sales System built for the raspberry-pi
 
 # System Requirements
 
-1. Node version 8.11.4
+1. Node version 12.13.0
 
 # Installation Guide
 
 1. `git clone git@github.com:SheepD/pipos.git`
-2. `cd pipos`
-3. `npm install`
-4. `npm run start`
+1. `cd pipos`
+1. `npm install`
+1. `npm run serve`
+1. `npm run start`
 
 # Contributing
 The author isnt accepting contributions as of now
--- main.js
+++ main.js
@@ -1,37 +1,40 @@
-const { app, BrowserWindow } = require("electron")
+const { app, BrowserWindow, Menu } = require("electron")
 
 let win
 
-function createWindow () {
+function createWindow() {
+  // hide the menu bar
+  Menu.setApplicationMenu(null)
+
   // create browser window
   win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true,
-    },
+      nodeIntegration: true
+    }
   })
 
+  const url = "http://localhost:8080/"
+
+  if (process.env.NODE_ENV === "dev") {
+    // open the dev tools
+    win.webContents.openDevTools()
+  }
+
   // load index.html into the browser window
-  win.loadFile("index.html")
-
-  // open the dev tools
-  // TODO: add condition to only open if in development mode
-  win.webContents.openDevTools()
-
-  // hide the menu bar
-  win.removeMenu()
+  win.loadURL(url)
 
   // gets triggered when the window is closed
   win.on("closed", () => {
     // dereference the window object to delete them
     // if the app supports multiple windows, win would be an array
     // and each window in the array should be deleted
-    win = null
-  })
+    win = null;
+  });
 }
 
-app.on("ready", createWindow)
+app.on("ready", createWindow);
 
 app.on("window-all-closed", () => {
   // this is for macOS
